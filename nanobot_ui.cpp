06a6cfd44807a6105f5e0037e8706be2c1122f2c
fedir <f.kovalyoff@tutanota.de>
2026-05-18 20:18:29 +0200

    fixed bugs in tictatoe

--- nanobot_ui.cpp
+++ nanobot_ui.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-enum signs_t { X, O, Empty };
+enum signs_t { X = 'X', O = 'O', Empty = '-' };
 
 using board_t = std::vector<std::vector<signs_t>>;
 
@@ -21,22 +21,25 @@ int main() {
   board_t board = board_make();
 
   signs_t player = X;
-  bool game = true;
-  while (game) {
+  bool is_finished = false;
+  int turns = 0;
+  while (!is_finished) {
 
     for (int i = 0; i != 3; i++) {
       for (int j = 0; j != 3; j++) {
-        std::cout << board[i][j];
+        std::cout << (char)board[i][j];
       }
       std::cout << "\n";
     }
+
     if (player == X) {
       player = O;
     } else if (player == O) {
       player = X;
     }
+
     int i, j;
-    std::cout << "Entr player " << player << " : ";
+    std::cout << "Entr player " << (char)player << " : ";
 
     std::cin >> i >> j;
     if (i > 3 && i > 3 || j > 3 && j > 3) {
@@ -46,10 +49,53 @@ int main() {
       }
     }
 
+    while (board[i][j] != Empty) {
+      std::cout << "Not corect";
+      std::cin >> i >> j;
+    }
     board[i][j] = player;
 
-    for () {
+    for (int k = 0; k < 3; k++) {
+      if (board[k][0] == player && board[k][1] == player &&
+          board[k][2] == player) {
+        is_finished = true;
+      }
     }
+
+    for (int k = 0; k < 3; k++) {
+      if (board[0][k] == player && board[1][k] == player &&
+          board[2][k] == player) {
+        is_finished = true;
+      }
+    }
+
+    bool is_diagonal1_full =
+        board[0][0] == player && board[1][1] == player && board[2][2] == player;
+    bool is_diagonal2_full =
+        board[0][2] == player && board[1][1] == player && board[2][0] == player;
+
+    if (is_diagonal1_full || is_diagonal2_full) {
+      is_finished = true;
+    }
+
+    if (turns == 8) {
+      break;
+    }
+
+    turns++;
+  }
+
+  for (int i = 0; i != 3; i++) {
+    for (int j = 0; j != 3; j++) {
+      std::cout << (char)board[i][j];
+    }
+    std::cout << "\n";
+  }
+
+  if (is_finished) {
+    std::cout << "Player " << (char)player << " win (@^v^)!!!\n";
+  } else {
+    std::cout << "Tie >_<\n";
   }
 
   return 0;
